97ce2daacab9d5fa9ec5ade172f55a2d8f4288ed
enicio <jessandro42@gmail.com>
2021-07-18 18:15:09 -0300

    Redux configurado

---
+++ src/redux/features/cadastreSlice.js
@@ -0,0 +1,90 @@
+import { createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import { getRegisters, postRegister } from '../../../service/service';
+
+export const fetchRegisters = createAsyncThunk(
+  'users/fetchRegisters',
+  async (_userId, thunkAPI) => {
+    const response = await getRegisters();
+    return response
+  }
+)
+
+export const createRegister = createAsyncThunk(
+  'users/createRegister',
+  async (inputField, { rejectWithValue }) => {
+    try {
+      const response = await postRegister(inputField);
+      return response
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  }
+)
+
+const initialState = {
+  value: 0,
+  registersList: [],
+  currentRequestId: "",
+  loading: false,
+  error: false,
+  created: false,
+  refresh: false,
+  renderDetail: false,
+  idToDetailRender: ''
+}
+
+export const registerSlice = createSlice({
+  name: 'counterType',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1
+    },
+    decrement: (state) => {
+      if(state.value <= 1) {
+        state.value = 0
+      } else {
+        state.value -= 1;
+      }
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload
+    },
+    renderDetails: (state, action) => {
+      state.renderDetail = !state.renderDetail
+      state.idToDetailRender = action.payload
+    }
+  },
+  extraReducers: {
+    [fetchRegisters.fulfilled]: (state, action) => {
+      state.registersList = [...action.payload]
+      state.loading = false
+    },
+    [fetchRegisters.pending]: (state, action) => {
+      state.loading = true
+    },
+    [fetchRegisters.rejected]: (state, action) => {
+      state.error = true
+    },
+    [createRegister.pending]: (state, { meta }) => {
+      // state.registersList = [...action.payload]
+      state.currentRequestId = meta;
+      state.created = true
+      // state.loading = true
+    },
+    [createRegister.fulfilled]: (state, action) => {
+      // state.registersList = [...action.payload]
+      if(state.created) {
+        state.refresh = !state.refresh;
+        state.created = false;
+      }
+      // state.loading = false
+    }
+  }
+
+})
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount, renderDetails } = registerSlice.actions
+
+export default registerSlice.reducer
---
+++ src/service/service.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+
+const URL = 'https://apideprodutos.vercel.app/api/petproducts';
+
+const instance = axios.create({
+  baseURL: "https://apideprodutos.vercel.app",
+});
+
+const instanceTransations = axios.create({
+  baseURL: "http://localhost:5000",
+});
+
+export const getProducts = async () => {
+  const { data } = await instance.get('/api/petproducts')
+                          .then((response) => response);
+  console.log(data)
+  return data;
+};
+
+export const getRegisters = async () => {
+  const { data } = await instanceTransations.get('/')
+                          .then((response) => response);
+  console.log(data)
+  return data;
+};
+
+export const postRegister = async (inputField) => {
+  const { data } = await instanceTransations.post('/api/v1/transacao', {
+    "estabelecimento": inputField.cnpj,
+    "cliente": inputField.cpf,
+    "valor": inputField.valor,
+    "descricao": inputField.description,
+  }).then((response) => response);
+
+  console.log('LOGO ANTES', inputField)
+  console.log(data);
+  return data;
+};
+
+
+export const fetchProduts = async () => {
+  const results = await fetch(URL);
+  const data = await results.json();
+  return data;
+};
+
+export const convertCurrency = (price, discount = 0) => {
+  return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' })
+  .format((price/100) - ( price/100 * discount))
+}
